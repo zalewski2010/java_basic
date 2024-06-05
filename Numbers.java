838707538c26a95ba28f5c536e61b6a2ac065814
Krzysztof Zalewski <zalewski2010@gmail.com>
2024-06-05 23:41:42 +0200

    2024-05-06

---
+++ Numbers.java
@@ -0,0 +1,21 @@
+public class Numbers {
+    public static void main(String[]args)
+            {
+                for (int i=1; i <= 100; i++)
+                System.out.println(i);
+                
+                {
+                    int skonta = 1000;
+                skonta = skonta + 100;
+                skonta = skonta - 200;
+
+                System.out.println("Na końcie zostało: " + skonta + "PLN");
+                }
+                
+                
+            }
+            
+
+
+}
+
