00884d63c2efaaeaa136ffc57977891cdadb8215
Krzysztof Zalewski <zalewski2010@gmail.com>
2024-06-06 21:58:52 +0200

    2024-06-05

---
+++ LitLicz.java
@@ -0,0 +1,12 @@
+public class LitLicz{
+    public static void main(String[] args)
+    {
+        System.out.println(10 + 0x10);
+        System.out.println(10 / 3);
+        System.out.println(10. / 3);
+        System.out.println(10d / 3);
+        System.out.println(2147483648L);
+        System.out.println(2147483647 + 1);
+        System.out.println(2147483647L + 1);
+    }
+}
