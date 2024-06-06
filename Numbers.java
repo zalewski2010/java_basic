23e069844edb297bdb77fc2071e675dd3c68d18e
Krzysztof Zalewski <zalewski2010@gmail.com>
2024-06-07 01:36:53 +0200

    a

--- Numbers.java
+++ Numbers.java
@@ -2,7 +2,9 @@ public class Numbers {
     public static void main(String[]args)
             {
                 for (int i=1; i <= 100; i++)
-                System.out.println(i);
+                {
+                    System.out.println(i);
+                }
                 
                 {
                     int skonta = 1000;
